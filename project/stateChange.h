c82d4afc48f2e3c50dda75de9675ac8633d85fca
student <akarellano2@miners.utep.edu>
2020-11-01 15:17:14 -0700

    change stateH

---
+++ project/stateChange.h
@@ -0,0 +1,17 @@
+#include switches_included
+#define switches_included
+#define SWITCHES (SW1 | SW2 | SW3 | SW4)
+#define SW1 BIT0
+#define SW2 BIT1
+#define SW3 BIT2
+#define SW4 BIT3
+
+void init_switch();
+
+void interrupt_handler();
+
+extern char switch_down;
+extern char switch_state;
+extern char state;
+
+#endif
