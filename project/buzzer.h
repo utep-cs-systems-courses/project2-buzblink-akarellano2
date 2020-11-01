e216e73dbb5e45bb86c8c865eb92b490d0ecdf87
student <akarellano2@miners.utep.edu>
2020-11-01 15:00:45 -0700

    buzzerH

---
+++ project/buzzer.h
@@ -0,0 +1,16 @@
+#ifdef buzzer_included
+#define buzzer_included
+
+void init_buzzer();
+
+void periodOfBuzzer(short cycle);
+
+void pattern1();
+
+void pattern2();
+
+void pattern3();
+
+void pattern4();
+
+#endif
