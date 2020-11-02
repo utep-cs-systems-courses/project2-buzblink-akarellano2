87b2514d5fb639a2dbc97e2da69740a55377b846
student <akarellano2@miners.utep.edu>
2020-11-01 21:49:22 -0700

    switch

---
+++ project/src/switches.c
@@ -0,0 +1,53 @@
+#include <msp430.h>
+#include "stateMachine.h"
+#include "led.h"
+#include "switches.h"
+#include "buzzer.h"
+
+char state;
+char switch_down;
+char switch_state;
+
+static char switch_update_interrupt_sense()
+{
+  char p2val = P2IN;
+
+  P2IES |= (p2val & SWITCHES);
+  P2IES &= (p2val | ~SWITCHES);
+
+  return p2val;
+}
+
+void switch_init()
+{
+  P2REN |= SWITCHES;
+  P2IE |= SWITCHES;
+  P2OUT |= SWITCHES;
+  P2DIR &= ~SWITCHES;
+
+  switch_update_interrupt_sense();
+  switch_interrupt_handler();
+}
+
+void switch_interrupt_handler()
+{
+  char p2val = switch_update_interrupt_sense();
+
+  if(p2val & SW1 ? 0 : 1){
+    state = 1;
+  }
+
+  else if(p2val & SW2 ? 0 : 1){
+    state = 2;
+  }
+
+  else if(p2val & SW3 ? 0 : 1){
+    state = 3;
+  }
+
+  else if(p2val & SW4 ? 0 : 1){
+    state = 4;
+  }
+
+  next_state();
+}
