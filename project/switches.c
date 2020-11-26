9f8c3d23deedc87825170c3395e4dacb96470c17
student <akarellano2@miners.utep.edu>
2020-11-26 11:03:41 -0700

    fixed switches to move state machine

--- project/switches.c
+++ project/switches.c
@@ -1,26 +1,27 @@
 #include <msp430.h>
-#include "stateMachine.h"
-#include "led.h"
 #include "switches.h"
+#include "led.h"
+#include "stateMachine.h"
 #include "buzzer.h"
 #include "libTimer.h"
 
 //char state;
-char switch_down;
-char switch_state;
-char s1, s2, s3, s4;
+char switch_state_down;
+char switch_state_changed;
+char B1, B2, B3, B4;
 
-static char switch_update_interrupt_sense()
+
+static char switch_update_interrupt_sense() // will update the interrupt depending on buttons
 {
   char p2val = P2IN;
 
-  P2IES |= (p2val & SWITCHES);
-  P2IES &= (p2val | ~SWITCHES);
+  P2IES |= (p2val & SWITCHES); // if up then sense down
+  P2IES &= (p2val | ~SWITCHES); // if down then sense up
 
   return p2val;
 }
 
-void switch_init()
+void switch_init() // initialize switch
 {
   P2REN |= SWITCHES;
   P2IE |= SWITCHES;
@@ -36,36 +37,37 @@ void switch_interrupt_handler()
 {
   char p2val = switch_update_interrupt_sense();
 
-  s1 = (p2val & SW1) ? 0 : 1;
-  s2 = (p2val & SW2) ? 0 : 1;
-  s3 = (p2val & SW3) ? 0 : 1;
-  s4 = (p2val & SW4) ? 0 : 1;
+  B1 = (p2val & SW1) ? 0 : 1;
+  B2 = (p2val & SW2) ? 0 : 1;
+  B3 = (p2val & SW3) ? 0 : 1;
+  B4 = (p2val & SW4) ? 0 : 1;
 
-  if(s1){
-    switch_down = s1;
-    switch_state = 1;
+  if(B1){
+    switch_state_down = B1;
+    switch_state_changed = 1;
     led_update();
     pattern1();
   }
 
-  else if(s2){
-    switch_down = s2;
-    switch_state = 2;
+  if(B2){
+    switch_state_down = B2;
+    switch_state_changed = 2;
     led_update();
     pattern2();
   }
-  else if(s3){
-    switch_down = s3;
-    switch_state = 3;
+  if(B3){
+    switch_state_down = B3;
+    switch_state_changed = 3;
     led_update();
     pattern3();
   }
-  else if(s4){
-    switch_down = s4;
-    switch_state = 4;
+  if(B4){
+    switch_state_down = B4;
+    switch_state_changed = 4;
     led_update();
     pattern4();
   }
-  switch_state = 1;
+  
+  switch_state_changed = 1;
   led_update();
 }
--- project/switches.h
+++ project/switches.h
@@ -1,17 +1,18 @@
 #ifndef switches_included
 #define switches_included
-#define SWITCHES (SW1 | SW2 | SW3 | SW4)
+
 #define SW1 BIT0
 #define SW2 BIT1
 #define SW3 BIT2
 #define SW4 BIT3
 
+#define SWITCHES (SW1 | SW2 | SW3 | SW4)
+
 void switch_init();
 void switch_interrupt_handler();
 
-extern char switch_down;
-extern char switch_state;
-extern char s1, s2, s3, s4;
+extern char switch_state_down, switch_state_changed;
+extern char B1, B2, B3, B4;
 //extern char state;
 
 #endif
