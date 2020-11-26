29c0be62b83649ba8676a9ee958ffc590f90aea3
student <akarellano2@miners.utep.edu>
2020-11-25 17:13:57 -0700

    interrupt handler

--- project/wdInterruptHandler.c
+++ project/wdInterruptHandler.c
@@ -1,15 +1,28 @@
 #include <msp430.h>
 #include "stateMachine.h"
+#include "switches.h"
+#include "buzzer.h"
+#include "led.h"
 
 void
 {
-  __interrupt_vec(WDT_VECTOR) WDT() { //250 SEC
+  __interrupt_vec(WDT_VECTOR) WDT(){ /*250 interrupts per second */
 
-    static char counter; //counter for blink
+    static char blink_count = 0;
+    if(++blink_count == 2 && S1){
+      dim_blink();
+      periodOfBuzzer(0);
+    }
 
-    if(counter++ == 125) {
-      next_state(); // will make sure it goes to next state //state_advance
+    if(blink_count ==125 && S3){
+      state_advance();
+    }
 
-      counter = 0;
+    if(S4){
+      periodOfBuzzer(0);
+      red_on = 0;
+      led_changed =1;
+      led_update();
+      blink_count = 0;
     }
   }
